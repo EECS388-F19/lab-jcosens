66d95d916de53eb451aafd8081e71fa4e52621b0
jcosens <jcosens@umich.edu>
2019-09-09 15:31:38 -0400

    Completed lab0

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+import random
+
+print("Jenny")
+num1 = random.randint(0,101)
+print(num1)
+num2 = random.randint(0,101)
+print(num2)
+sum = num1+num2
+print("Sum = ", sum)
+print("Average = ", sum/2.0)
